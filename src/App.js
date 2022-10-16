8e9490d21c5390a6c3be1e562ce2d5629f3b05d2
putukus <putu@codekiwi.com>
2022-10-16 21:37:03 +0800

    add contract address and abi file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
 import React, { useEffect, useState, useCallback } from "react";
+import { ethers } from "ethers";
 import "./App.css";
+import abi from "./utils/WavePortal.json";
+
 
 const getEthereumObject = () => window.ethereum;
 
 function App() {
-  
   const [currentAccount, setCurrentAccount] = useState("");
+
+  /**
+   * Create a variable here that holds the contract address after you deploy!
+   */
+  const contractAddress = "0x1c37BE624c899c056e1d16bA367e145f22652dF8";
+
+  /**
+   * Create a variable here that references the abi content!
+   */
+  const contractABI = abi.abi;
+
+
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+
+        /*
+        * Execute the actual wave from your smart contract
+        */
+         const waveTxn = await wavePortalContract.wave();
+         console.log("Mining...", waveTxn.hash);
+ 
+         await waveTxn.wait();
+         console.log("Mined -- ", waveTxn.hash);
+ 
+         count = await wavePortalContract.getTotalWaves();
+         console.log("Retrieved total wave count...", count.toNumber());
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   /*
    * This function returns the first linked account found.
@@ -80,7 +125,7 @@ function App() {
           cool right? Connect your Ethereum wallet and wave at me!
         </div>
 
-        <button className="waveButton" onClick={null}>
+        <button className="waveButton" onClick={wave}>
           Wave at Me
         </button>
 
